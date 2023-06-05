24342dcdce5cf22e257577be67942a3d83c1572a
Juan Jose Marin Molina <Hermanomayor2@gmail.com>
2023-06-05 14:02:40 +0200

    prueba3

--- zapateria/src/modules/auth/auth.controller.ts
+++ zapateria/src/modules/auth/auth.controller.ts
@@ -3,14 +3,25 @@ import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { LoginUserDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // @Post('register')
+  // create(@Body() createAuthDto: CreateUserDto) {
+  //   return this.authService.create(createAuthDto);
+  // }
+
   @Post('register')
-  create(@Body() createAuthDto: CreateUserDto) {
-    return this.authService.create(createAuthDto);
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.authService.create(createUserDto);
+  }
+
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
   }
 
   @Get()
