201e46df93a0387b41ca47e4b86e303f6504f7a8
Juan Jose Marin Molina <Hermanomayor2@gmail.com>
2023-06-05 13:07:31 +0200

    prueba cambios

--- zapateria/src/modules/auth/strategies/jwt.strategy.ts
+++ zapateria/src/modules/auth/strategies/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { InjectRepository } from "@nestjs/typeorm";
-import { ExtractJwt, Strategy } from "passport-jwt";
+import { Strategy } from "passport-jwt";
+import { ExtractJwt } from "passport-jwt";
 import { Repository } from "typeorm";
 import { User } from "../entities/user.entity";
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
